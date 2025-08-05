336842c07bb39d7ea52285b1fd5923561bdc44ea
VivekNandimandalam <viveknandimandalam463@gmail.com>
2025-08-06 01:03:03 +0530

    Progress 5th August

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+# llm = ChatOpenAI(model="gpt-4o-mini")
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+response = llm.invoke("Hello, how are you?")
+print(response)
+
+
+    
